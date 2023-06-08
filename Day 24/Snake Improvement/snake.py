b566797ad40dab0ffcfa446c0ef0b48210abf659
zermpinos <130582198+zermpinos@users.noreply.github.com>
2023-06-08 22:07:15 +0300

    Create snake.py

---
+++ Day 24/Snake Improvement/snake.py
@@ -0,0 +1,165 @@
+import turtle
+import time
+import random
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+SNAKE_COLOR = "white"
+SNAKE_SHAPE = "square"
+FOOD_COLOR = "blue"
+FOOD_SHAPE = "circle"
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = turtle.Turtle(SNAKE_SHAPE)
+        new_segment.color(SNAKE_COLOR)
+        new_segment.penup()
+        new_segment.speed("fastest")
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def grow(self):
+        self.add_segment(self.segments[-1].position())
+
+
+class Food(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape(FOOD_SHAPE)
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color(FOOD_COLOR)
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+
+
+class Scoreboard(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = self.load_high_score()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.save_high_score()
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def save_high_score(self):
+        with open("high_score.txt", "w") as file:
+            file.write(str(self.high_score))
+
+    def load_high_score(self):
+        try:
+            with open("high_score.txt", "r") as file:
+                return int(file.read())
+        except FileNotFoundError:
+            return 0
+
+
+
+def main():
+    screen = turtle.Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My Snake Game")
+    screen.tracer(0)
+
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
+
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+
+        snake.move()
+
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.grow()
+            scoreboard.increase_score()
+
+        if (
+            snake.head.xcor() > 290
+            or snake.head.xcor() < -290
+            or snake.head.ycor() > 290
+            or snake.head.ycor() < -290
+        ):
+            game_is_on = False
+            scoreboard.game_over()
+
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
+
+    screen.mainloop()
+
+
+main()
