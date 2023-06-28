c3a6401e58a8215817f38a3e861d5f0e12ce1e48
zermpinos <130582198+zermpinos@users.noreply.github.com>
2023-06-28 20:56:31 +0300

    Update linkedin_automated_applications.py

--- Day 49/LinkedIn Automated Applications/linkedin_automated_applications.py
+++ Day 49/LinkedIn Automated Applications/linkedin_automated_applications.py
@@ -4,17 +4,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-email = 'Your Email'
-password = r"Your Password'
-phone = 'Your Phone'
+email = 'panoszermpinos@gmail.com'
+password = r"kÙÀÚÊ]ðÅ+½#+ç¼µ×UÙ·áðÎn£\ÓnÓ»³la´æf^qQ '"
+phone = '6974764432'
 
-chrome_driver_path = Service(r"Your Chrome Driver Path")
-brave_path = r"Your Brave Browser Path"
+chrome_driver_path = Service(r"G:\- Projects\Python Projects\Best\100 Days\-- CHROME-DRIVER --\chromedriver.exe")
+brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
 
 options = webdriver.ChromeOptions()
 options.binary_location = brave_path
 driver = webdriver.Chrome(service=chrome_driver_path, options=options)
-driver.get('https://www.linkedin.com/jobs/search/?currentJobId=3649796468&f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom')  # Can be any link with jobs
+driver.get('https://www.linkedin.com/jobs/search/?currentJobId=3649796468&f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom')
 
 sign_in_button = driver.find_element(By.LINK_TEXT, "Sign in")
 sign_in_button.click()
@@ -36,10 +36,20 @@ for listing in all_listings:
         if element.text == "Easy Apply":
             element.click()
             break
+    next_button = driver.find_elements(By.CSS_SELECTOR, '.artdeco-button__text')
+    for element in next_button:
+        if element.text == 'Next':
+            element.click()
+            break
+    review_button = driver.find_elements(By.CSS_SELECTOR, '.artdeco-button__text')
+    for element in review_button:
+        if element.text == 'Review':
+            element.click()
+            break
     submit_button = driver.find_elements(By.CSS_SELECTOR, '.artdeco-button__text')
-    for elements in submit_button:
-        if elements.text == "Submit application":
-            elements.click()
+    for element in submit_button:
+        if element.text == "Submit application":
+            element.click()
             break
     close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
     close_button.click()
