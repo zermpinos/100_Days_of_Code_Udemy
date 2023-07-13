47ef3387e17e191513404c13535f01520cffea41
zermpinos <130582198+zermpinos@users.noreply.github.com>
2023-07-13 12:57:48 +0300

    Create models.py

---
+++ Day 63/Favorite Book Database/books_app/models.py
@@ -0,0 +1,7 @@
+from . import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
